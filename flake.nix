37f80b3a7022b37599443c8f672f2566e57bb4a3
NotAShelf <raf@notashelf.dev>
2024-09-10 01:57:53 +0300

    flake: get rid of redundant destructuring

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [./pkgs];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {pkgs, ...}: {
