67c4dd7a111fb4c53a29ee11a7f611d6e9f9c7ec
NotAShelf <raf@notashelf.dev>
2024-09-10 02:03:12 +0300

    flake: support nix-systems

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Streamlined documentation generation for NixOS modules";
 
   inputs = {
+    systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
@@ -9,7 +10,7 @@
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [./pkgs];
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = import inputs.systems;
       perSystem = {pkgs, ...}: {
         formatter = pkgs.alejandra;
       };
