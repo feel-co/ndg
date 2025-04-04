9cdf88bf350ab70f87a50ba3c683b99184b064b5
NotAShelf <raf@notashelf.dev>
2025-04-05 01:19:39 +0300

    nix: install shell completions

--- nix/package.nix
+++ nix/package.nix
@@ -1,15 +1,31 @@
 {
   lib,
   rustPlatform,
+  installShellFiles,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ndg";
   version = "0.1.0";
 
+  nativeBuildInputs = [installShellFiles];
+
   # TODO: add a filter here
   src = ../.;
   cargoLock.lockFile = finalAttrs.src + /Cargo.lock;
   enableParallelBuilding = true;
+
+  postInstall = ''
+    # Generate manpages and completion files
+    $out/bin/ndg generate --output-dir ./dist
+
+    # Install shell completions
+    installShellCompletion --bash --name ndg.bash ./dist/completions/ndg.bash
+    installShellCompletion --fish --name ndg.fish ./dist/completions/ndg.fish
+    installShellCompletion --zsh --name _ndg ./dist/completions/_ndg
+
+    # Install manpages
+    installManPage ./dist/man/ndg.1
+  '';
 
   meta = {
     description = "ndg - not a docs generator";
