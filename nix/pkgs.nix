84418df9077efc7086764c89772419f9ca99b7a2
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2026-02-17 01:21:00 -0500

    nix: fix non-flakes usage

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -2,7 +2,7 @@ let
   lock = builtins.fromJSON (builtins.readFile ../flake.lock);
   nixpkgsNode = lock.nodes.${lock.nodes.root.inputs.nixpkgs}.locked;
   nixpkgs = fetchTarball {
-    url = "https://github.com/${nixpkgsNode.owner}/${nixpkgsNode.repo}/archive/${nixpkgsNode.rev}.tar.gz";
+    inherit (nixpkgsNode) url;
     sha256 = nixpkgsNode.narHash;
   };
 in
