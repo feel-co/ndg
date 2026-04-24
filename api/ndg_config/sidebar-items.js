a1fa7ef40db40fc76b943ef44b3a238dee99a043
NotAShelf <NotAShelf@users.noreply.github.com>
2026-04-24 21:13:38 +0000

    Deploying to gh-pages from @ feel-co/ndg@b5330a3aa6d6850d4cebcb832ee7e85f25534194 🚀

--- api/ndg_config/sidebar-items.js
+++ api/ndg_config/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["anchor","assets","config","error","meta","postprocess","search","sidebar","templates"]};
+window.SIDEBAR_ITEMS = {"mod":["anchor","assets","config","error","index","meta","postprocess","search","sidebar","templates"]};
